061dccb64ccd4df3dfd9944d274ab9f7bd84917c
keisidoci <keisidoci12@gmail.com>
2023-08-31 12:48:34 +0200

    AWS Serverless framework

---
+++ src/database/dbConfig.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+require('dotenv').config(); 
+
+let conn = null;
+const mongoURI = process.env.DB;
+
+module.exports = connectDatabase = async () => {
+  try {
+    if (conn == null) {
+      console.log("Creating new connection to database...");
+      console.log(mongoURI);
+      conn = await mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      return conn;
+    }
+    console.log("Connection already established, reusing the connection...");
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
+};
